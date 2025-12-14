571511dec82803f039196e843e20d8e6fb416138
nebkoo <46592476+nebkoo@users.noreply.github.com>
2025-12-14 21:10:23 +0800

    feat: Added raydium clmm and raydium cpmm decoder parsing (#156)

---
+++ decoders/raydium-clmm-decoder/src/types/pool_status_bit_flag.rs
@@ -0,0 +1,10 @@
+use alloc::format;
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+pub enum PoolStatusBitFlag {
+    Enable,
+    Disable,
+}
---
+++ decoders/raydium-clmm-decoder/src/types/pool_status_bit_index.rs
@@ -0,0 +1,13 @@
+use alloc::format;
+use carbon_core::{borsh, CarbonDeserialize};
+
+#[derive(
+    CarbonDeserialize, Debug, serde::Serialize, serde::Deserialize, PartialEq, Eq, Clone, Hash,
+)]
+pub enum PoolStatusBitIndex {
+    OpenPositionOrIncreaseLiquidity,
+    DecreaseLiquidity,
+    CollectFee,
+    CollectReward,
+    Swap,
+}
